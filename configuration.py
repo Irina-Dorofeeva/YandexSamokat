f6759f50a0fe2f50e354ac8bbc73c2324a94d135
Irina-Dorofeeva <irina.v.d0r0feeva@gmail.com>
2024-10-20 20:27:42 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE = "https://8f3f40fd-bac7-429d-b883-4a9de229bbc2.serverhub.praktikum-services.ru"
+ORDERS = "/api/v1/orders"
---
+++ sender_stand_request.py
@@ -0,0 +1,19 @@
+# Ирина Дорофеева, 22-я когорта — Финальный проект. Инженер по тестированию плюс
+import requests
+import configuration
+import data
+
+def create_order(body):
+    return requests.post (configuration.URL_SERVICE + configuration.ORDERS, json=body)
+
+def get_order(track_id):
+    get_order_url = configuration.URL_SERVICE + configuration.ORDERS + "/track?t=" + str(track_id)
+    response = requests.get(get_order_url)
+    return response
+
+def test_create_order_and_get_order_information():
+    response = create_order(data.order_body)
+    track_id = response.json()["track"]
+    order_response = get_order(track_id)
+    assert order_response.status_code == 200, "Ошибка: " + str(order_response.status_code)
+    order_data = order_response.json()
